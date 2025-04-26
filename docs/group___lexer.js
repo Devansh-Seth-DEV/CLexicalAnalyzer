ea5b8b25a1b1c59e01a96682a4eba3c82c565312
Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
2025-04-26 23:35:32 +0530

    Delete docs directory

--- docs/group___lexer.js
+++
@@ -1,7 +0,0 @@
-var group___lexer =
-[
-    [ "File I/O", "group___lexer_i_o.html", "group___lexer_i_o" ],
-    [ "Tokenization", "group___tokenization.html", "group___tokenization" ],
-    [ "Token Validation", "group___token_validation.html", "group___token_validation" ],
-    [ "lexer_tokenize.h", "lexer__tokenize_8h.html", null ]
-];
--- docs/lexer__tokenize_8h.js
+++
@@ -1,6 +0,0 @@
-var lexer__tokenize_8h =
-[
-    [ "tokcnt", "group___tokenization.html#gae0c0a7a1181c1edc46cd937176f4c37c", null ],
-    [ "toknz", "group___tokenization.html#ga221db00549e4cd6c05c5e809b13bce26", null ],
-    [ "toknz_segtoset", "group___tokenization.html#ga743b8789dd6718aae4459fbd0b5cba95", null ]
-];
